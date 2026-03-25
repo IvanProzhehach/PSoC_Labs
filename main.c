88f666f2ce0939d7279f966e28101b32ed3d99d4
IvanProzhehach <164470132+IvanProzhehach@users.noreply.github.com>
2026-03-25 11:35:47 +0200

    code

---
+++ main.c
@@ -0,0 +1,31 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        if(Button_Read() == 1) {
+            LED_Write(0);
+        } else {
+        LED_Write(1);
+        }
+    
+    }
+}
+
+/* [] END OF FILE */
